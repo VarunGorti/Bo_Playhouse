f51e1917a5c044ea90a60739507e2cf80232d48a
Bo <boyang.deng@gmail.com>
2019-11-02 12:17:21 -0500

    create bop it activity

---
+++ app/src/main/java/com/example/bodeng/boplayhouse/BopItActivity.java
@@ -0,0 +1,13 @@
+package com.example.bodeng.boplayhouse;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BopItActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bop_it);
+    }
+}
